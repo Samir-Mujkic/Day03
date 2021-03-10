d96fd952c59fd14acc7c154a0e48f29ebfdd7e25
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-10 12:26:34 +0100

    Zadatak 2

---
+++ ex02/src/index.js
@@ -0,0 +1,21 @@
+function main () {
+
+    // This is a single line JavaScript comment    
+    // This is a single line JavaScript comment
+    // This is a single line JavaScript comment
+    // Thsi is a single line JavaScript commnet
+    // This is a single line JavaScript comment
+
+    /* document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    */
+
+
+
+}
+
+console.log(main());
+module.exports = main;
