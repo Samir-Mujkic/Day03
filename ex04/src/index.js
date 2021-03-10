e0de449de73c5f94afba17226730fb7ec8866aa4
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-10 13:10:15 +0100

    zadatak 4

---
+++ ex04/src/index.js
@@ -0,0 +1,26 @@
+function main(){
+    // Only change code below this line
+   
+  var x = 10;
+  
+   x=y;
+
+  return [x , y];
+   
+
+
+
+
+
+
+
+
+
+   //Only change code above this line
+   
+   
+   
+   }
+   
+   console.log(main());
+   module.exports = main;
